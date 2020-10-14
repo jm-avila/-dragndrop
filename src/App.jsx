2c2f49deca860db4892005252255b7184c7d36b3
jm-avila <jm.avila1309@icloud.com>
2020-10-14 13:29:06 +0200

    Dynamically generating boxes and dragged element

--- src/App.jsx
+++ src/App.jsx
@@ -3,57 +3,27 @@ import React, { Fragment, useRef } from "react";
 import "./App.css";
 
 function App() {
-  const draggedElement = useRef(null);
+  const draggedElementRef = useRef(null);
+  const draggedElement = (
+    <div className="fill" draggable={true} ref={draggedElementRef}></div>
+  );
 
   return (
     <Fragment>
-      <div
-        className="empty"
-        onDragStart={handletDragStart}
-        onDragEnd={handleDragEnd}
-        onDragOver={handleDragOver}
-        onDragEnter={handleDragEnter}
-        onDragLeave={handleDragLeave}
-        onDrop={(e) => handleDrop(e, draggedElement)}
-      >
-        <div className="fill" draggable={true} ref={draggedElement}></div>
-      </div>
-      <div
-        className="empty"
-        onDragStart={handletDragStart}
-        onDragEnd={handleDragEnd}
-        onDragOver={handleDragOver}
-        onDragEnter={handleDragEnter}
-        onDragLeave={handleDragLeave}
-        onDrop={(e) => handleDrop(e, draggedElement)}
-      ></div>
-      <div
-        className="empty"
-        onDragStart={handletDragStart}
-        onDragEnd={handleDragEnd}
-        onDragOver={handleDragOver}
-        onDragEnter={handleDragEnter}
-        onDragLeave={handleDragLeave}
-        onDrop={(e) => handleDrop(e, draggedElement)}
-      ></div>
-      <div
-        className="empty"
-        onDragStart={handletDragStart}
-        onDragEnd={handleDragEnd}
-        onDragOver={handleDragOver}
-        onDragEnter={handleDragEnter}
-        onDragLeave={handleDragLeave}
-        onDrop={(e) => handleDrop(e, draggedElement)}
-      ></div>
-      <div
-        className="empty"
-        onDragStart={handletDragStart}
-        onDragEnd={handleDragEnd}
-        onDragOver={handleDragOver}
-        onDragEnter={handleDragEnter}
-        onDragLeave={handleDragLeave}
-        onDrop={(e) => handleDrop(e, draggedElement)}
-      ></div>
+      {Array.from({ length: 5 }, (...args) => (
+        <div
+          className="empty"
+          key={args[1]}
+          onDragStart={handletDragStart}
+          onDragEnd={handleDragEnd}
+          onDragOver={handleDragOver}
+          onDragEnter={handleDragEnter}
+          onDragLeave={handleDragLeave}
+          onDrop={(e) => handleDrop(e, draggedElementRef)}
+        >
+          {!args[1] && draggedElement}
+        </div>
+      ))}
     </Fragment>
   );
 }
